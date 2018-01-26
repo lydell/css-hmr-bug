c775f610fd9a9e06c94862a1d35dcd0ab928a59a
Simon Lydell <simon.lydell@gmail.com>
2018-01-26 09:27:09 +0100

    Improve the example by actually using the class

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import styles from "./index.css";
 
 console.log("CSS classes", styles);
+
+const test = document.createElement("p");
+test.textContent = "test";
+test.className = styles.test;
+
+document.body.appendChild(test);
